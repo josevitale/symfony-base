247d197556c70885cbc7cf08f11e66a1cd61fb36
José María Vitale <josevitale@gmail.com>
2018-02-20 14:27:58 -0300

    Clase ErrorException y plantilla error.html.twig

---
+++ src/AppBundle/Exception/ErrorException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Exception;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use AppBundle\Response\ResponseError;
+
+class ErrorException extends HttpException
+{
+    protected $responseError;
+
+    public function __construct(ResponseError $responseError, \Exception $previous = null, array $headers = array(), $code = 0)
+    {
+        $this->responseError = $responseError;
+        parent::__construct($responseError->getStatusCode(), $responseError->getTitulo(), $previous, $headers, $code);
+    }
+
+    public function getResponseError()
+    {
+        return $this->responseError;
+    }
+}
