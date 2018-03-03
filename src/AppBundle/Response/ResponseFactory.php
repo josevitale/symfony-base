b317c34890e12d94932200089e313607b88450b4
José María Vitale <josevitale@gmail.com>
2018-03-03 11:14:05 -0300

    Parametrización del template para mostrar página de error

--- src/AppBundle/Response/ResponseFactory.php
+++ src/AppBundle/Response/ResponseFactory.php
@@ -34,19 +34,20 @@ class ResponseFactory
 
     protected function crearHtmlResponse(Request $request, ResponseData $responseData)
     {
+        $controllerRequest = explode("Controller::", $request->attributes->get('_controller'));
+        $controllerRequest[0] = explode("\\", $controllerRequest[0]);
+        $controllerRequest[0] = $controllerRequest[0][count($controllerRequest[0]) - 1];
+        $controller = array(
+            'controller' => strtolower($controllerRequest[0]),
+            'action' => str_replace('Action', '', $controllerRequest[1]),
+        );
+        $controllerConfig = Yaml::parseFile($this->container->get('kernel')->getRootDir() . '/Resources/config/controller/' . $controller['controller'] . '.yml');
+        $view = $controllerConfig[$controller['action']]['template'];
         if ($responseData instanceof ResponseError) {
             $view = 'error.html.twig';
-        }
-        else {
-            $controllerRequest = explode("Controller::", $request->attributes->get('_controller'));
-            $controllerRequest[0] = explode("\\", $controllerRequest[0]);
-            $controllerRequest[0] = $controllerRequest[0][count($controllerRequest[0]) - 1];
-            $controller = array(
-                'controller' => strtolower($controllerRequest[0]),
-                'action' => str_replace('Action', '', $controllerRequest[1]),
-            );
-            $controllerConfig = Yaml::parseFile($this->container->get('kernel')->getRootDir() . '/Resources/config/controller/' . $controller['controller'] . '.yml');
-            $view = $controllerConfig[$controller['action']]['template'];
+            if (array_key_exists('template_error', $controllerConfig[$controller['action']])) {
+                $view = $controllerConfig[$controller['action']]['template_error'];
+            }
         }
         $renderView = $this->container->get('templating')->render($view, $responseData->toArray());
         $response = new Response($renderView, $responseData->getStatusCode());
