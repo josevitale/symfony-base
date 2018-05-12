879c941f003e665478d662c30464331136f604d9
José María Vitale <josevitale@gmail.com>
2018-05-12 10:35:10 -0300

    Clase ResponseMensaje

---
+++ src/AppBundle/Response/ResponseMensaje.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Response;
+
+class ResponseMensaje
+{
+    const ERROR = 'danger';
+    const SUCCESS = 'success';
+    const WARNING = 'warning';
+    const INFO = 'info';
+}
