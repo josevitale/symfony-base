ef5b5f1637293da204b47bad20fea04a3fd1c49b
José María Vitale <josevitale@gmail.com>
2018-05-12 09:26:50 -0300

    Validación de propiedad name

--- src/AppBundle/Entity/Group.php
+++ src/AppBundle/Entity/Group.php
@@ -5,10 +5,15 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\Group as BaseGroup;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_group")
+ * @UniqueEntity(
+ *     fields={"name"},
+ *     message="El nombre del grupo no debe existir en el sistema."
+ * )
  */
 class Group extends BaseGroup
 {
